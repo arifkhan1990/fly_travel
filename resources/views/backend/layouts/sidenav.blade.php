6b94891f0231ce24ca97f3bd90f808a7384e1aea
ripon <riponahmed2201@gmail.com>
2020-10-31 16:44:41 +0600

    All Controller and Model Created

--- resources/views/backend/layouts/sidenav.blade.php
+++ resources/views/backend/layouts/sidenav.blade.php
@@ -14,25 +14,25 @@
         <li>
             <a href="javascript:;" class="side-menu">
                 <div class="side-menu__icon"> <i data-feather="box"></i> </div>
-                <div class="side-menu__title"> Menu Layout <i data-feather="chevron-down" class="side-menu__sub-icon"></i> </div>
+                <div class="side-menu__title"> Administrator <i data-feather="chevron-down" class="side-menu__sub-icon"></i> </div>
             </a>
             <ul class="">
                 <li>
                     <a href="index.html" class="side-menu">
                         <div class="side-menu__icon"> <i data-feather="activity"></i> </div>
-                        <div class="side-menu__title"> Side Menu </div>
+                        <div class="side-menu__title"> Employee </div>
                     </a>
                 </li>
                 <li>
                     <a href="simple-menu-light-dashboard.html" class="side-menu">
                         <div class="side-menu__icon"> <i data-feather="activity"></i> </div>
-                        <div class="side-menu__title"> Simple Menu </div>
+                        <div class="side-menu__title"> User </div>
                     </a>
                 </li>
                 <li>
                     <a href="top-menu-light-dashboard.html" class="side-menu">
                         <div class="side-menu__icon"> <i data-feather="activity"></i> </div>
-                        <div class="side-menu__title"> Top Menu </div>
+                        <div class="side-menu__title"> Role </div>
                     </a>
                 </li>
             </ul>
@@ -40,50 +40,56 @@
         <li>
             <a href="side-menu-light-inbox.html" class="side-menu">
                 <div class="side-menu__icon"> <i data-feather="inbox"></i> </div>
-                <div class="side-menu__title"> Inbox </div>
+                <div class="side-menu__title"> Blog </div>
             </a>
         </li>
         <li>
             <a href="side-menu-light-file-manager.html" class="side-menu">
                 <div class="side-menu__icon"> <i data-feather="hard-drive"></i> </div>
-                <div class="side-menu__title"> File Manager </div>
+                <div class="side-menu__title"> Contact </div>
             </a>
         </li>
         <li>
             <a href="side-menu-light-point-of-sale.html" class="side-menu">
                 <div class="side-menu__icon"> <i data-feather="credit-card"></i> </div>
-                <div class="side-menu__title"> Point of Sale </div>
+                <div class="side-menu__title"> Shop </div>
             </a>
         </li>
         <li>
             <a href="side-menu-light-chat.html" class="side-menu">
                 <div class="side-menu__icon"> <i data-feather="message-square"></i> </div>
-                <div class="side-menu__title"> Chat </div>
+                <div class="side-menu__title"> Gallery </div>
             </a>
         </li>
         <li>
             <a href="side-menu-light-post.html" class="side-menu">
                 <div class="side-menu__icon"> <i data-feather="file-text"></i> </div>
-                <div class="side-menu__title"> Post </div>
+                <div class="side-menu__title"> Transaction </div>
             </a>
         </li>
         <li class="side-nav__devider my-6"></li>
         <li>
             <a href="javascript:;" class="side-menu">
                 <div class="side-menu__icon"> <i data-feather="edit"></i> </div>
-                <div class="side-menu__title"> Crud <i data-feather="chevron-down" class="side-menu__sub-icon"></i> </div>
+                <div class="side-menu__title"> Services <i data-feather="chevron-down" class="side-menu__sub-icon"></i> </div>
             </a>
             <ul class="">
                 <li>
                     <a href="side-menu-light-crud-data-list.html" class="side-menu">
                         <div class="side-menu__icon"> <i data-feather="activity"></i> </div>
-                        <div class="side-menu__title"> Data List </div>
+                        <div class="side-menu__title"> Hotel </div>
                     </a>
                 </li>
                 <li>
                     <a href="side-menu-light-crud-form.html" class="side-menu">
                         <div class="side-menu__icon"> <i data-feather="activity"></i> </div>
-                        <div class="side-menu__title"> Form </div>
+                        <div class="side-menu__title"> Visa </div>
+                    </a>
+                </li>
+                <li>
+                    <a href="side-menu-light-crud-form.html" class="side-menu">
+                        <div class="side-menu__icon"> <i data-feather="activity"></i> </div>
+                        <div class="side-menu__title"> Air Ticket </div>
                     </a>
                 </li>
             </ul>
