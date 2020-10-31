bab37de4bbc2c30fa2e299fc870f7055e7126601
ripon <riponahmed2201@gmail.com>
2020-10-31 16:19:26 +0600

    dashboard mastering

---
+++ resources/views/backend/layouts/footer_js.blade.php
@@ -0,0 +1,8 @@
+<!-- END: Dark Mode Switcher-->
+<!-- BEGIN: JS Assets-->
+<script src="{{asset('assets/admin/maps/documentation/javascript/examples/markerclusterer/markerclusterer.js')}}"></script>
+<script src="{{asset('assets/admin/maps/api/js?key=AIzaSyAjDvJ3gdZ92nvKNVUGMwCHeaRRU0k92pE&libraries=places')}}"></script>
+<script src="{{asset('assets/admin/dist/js/app.js')}}"></script>
+<!-- END: JS Assets-->
+
+@yield('custom_js')
